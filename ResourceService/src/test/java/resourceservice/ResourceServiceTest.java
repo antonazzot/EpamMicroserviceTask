45890801ed0c00166cbc2c052cc15dfcb5c09234
Anton_Tsyrkunou <anton.azzot@yandex.ru>
2022-04-22 19:53:13 +0300

    25-contract-test

---
+++ ResourceService/src/test/java/resourceservice/ResourceServiceTest.java
@@ -0,0 +1,34 @@
+package resourceservice;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+import resourceservice.controller.ResourceController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+classes = ResourceService.class)
+@DirtiesContext
+@AutoConfigureMessageVerifier
+public class ResourceServiceTest {
+
+    @Autowired
+    private ResourceController resourceController;
+
+    @Before
+    public void setup() {
+        StandaloneMockMvcBuilder standaloneMockMvcBuilder
+                = MockMvcBuilders.standaloneSetup(resourceController);
+        RestAssuredMockMvc.standaloneSetup(standaloneMockMvcBuilder);
+    }
+
+
+}
---
+++ SongService/src/test/java/songservice/controller/SongMetadataControllerTest.java
@@ -0,0 +1,20 @@
+package songservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SongMetadataControllerTest {
+
+    @Test
+    void saveMetadata() {
+    }
+
+    @Test
+    void deleteMetadata() {
+    }
+
+    @Test
+    void getSongsMetadata() {
+    }
+}
